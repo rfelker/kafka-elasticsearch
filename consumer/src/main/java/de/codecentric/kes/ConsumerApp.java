a818e6949f38ae370b93396a9c927c6935044741
Rolf Felker <rolf.felker@codecentric.de>
2016-04-04 10:26:36 +0200

    init

---
+++ consumer/src/main/java/de/codecentric/kes/ConsumerApp.java
@@ -0,0 +1,14 @@
+package de.codecentric.kes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class ConsumerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApp.class, args);
+    }
+}
